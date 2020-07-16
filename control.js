cc65939e10b6eff357f98fafc03e81def16474f3
smanewell <snewell5@my.bcit.ca>
2020-07-15 21:58:00 -0700

    fixed response route for pages with ejs

--- control.js
+++ control.js
@@ -8,6 +8,12 @@ server.set("view engine", "ejs");
 
 server.get('/', (req, res) => res.render("pages/index"));
 
-server.get('/about', (req, res) => res.render("./about.html"));
+server.get('/about', (req, res) => res.render("pages/about"));
+
+server.get('/projects', (req, res) => res.render("pages/projects"));
+
+server.get('/work_history', (req, res) => res.render("pages/work_history"));
+
+server.get('/gallery', (req, res) => res.render("pages/gallery"));
 
 server.listen(900);

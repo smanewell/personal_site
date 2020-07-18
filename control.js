eb8c1a4130fe85a85e0aa83e5c48ccf33a88685f
smanewell <snewell5@my.bcit.ca>
2020-07-17 20:59:45 -0700

    skeleton for gallery pages made

--- control.js
+++ control.js
@@ -16,9 +16,13 @@ server.get('/work_history', (req, res) => res.render("pages/work_history"));
 server.get('/gallery', (req, res) => res.render("pages/gallery"));
 
 server.get('/gallery/miniatures', (req, res) => res.render("pages/miniatures"));
+
 server.get('/gallery/character_sketches', (req, res) => res.render("pages/character_sketches"));
+
 server.get('/gallery/3D_models', (req, res) => res.render("pages/3D_models"));
+
 server.get('/gallery/sigils', (req, res) => res.render("pages/sigils"));
+
 server.get('/gallery/misc', (req, res) => res.render("pages/misc"));
 
 server.listen(900);
